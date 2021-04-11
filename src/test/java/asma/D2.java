7f3fa3d8b12dcbda1ab146caaebf741d86644c7c
asmat <asma.talukder@yahoo.com>
2021-04-11 14:50:33 -0400

    new save mine

---
+++ src/test/java/asma/D2.java
@@ -0,0 +1,8 @@
+package asma;
+
+public class D2 {
+	public static void main(String[]args) {
+		System.out.println(200);
+	}
+
+}
---
+++ src/test/java/shamim/D1.java
@@ -0,0 +1,10 @@
+package shamim;
+
+public class D1 {
+	public static void main(String []args) {
+		System.out.println(300);
+		System.out.println(200);
+	}
+	
+
+}
